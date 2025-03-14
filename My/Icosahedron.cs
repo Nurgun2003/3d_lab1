5570e181f7d7d9909f18b2a4f4bd60685348b26f
Nurgun2003 <90663118+Nurgun2003@users.noreply.github.com>
2025-03-15 02:26:33 +0900

    Add Icosahedron

---
+++ My/Icosahedron.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace lab1.My
+{
+    public class Icosahedron : ModelVisual3D
+    {
+        public Icosahedron() 
+        {
+            Draw(_size, _pos);
+        }
+        //размер по умолчанию
+        private double _size = 0.5;
+        //поле задания размера
+        public double Size
+        {
+            get => _size;
+            set
+            {
+                _size = value;
+                //после изменения значения поля фиксируем изменения
+                Draw(_size, _pos);
+            }
+        }
+        //положение центра октаэдра
+        private Point3D _pos;
+        public Point3D Position
+        {
+            get => _pos;
+            set
+            {
+                _pos = value;
+                //после изменения значения поля фиксируем изменения
+                Draw(_size, _pos);
+            }
+        }
+
+        //Добавление грани
+        private static GeometryModel3D AddFace(
+                        Point3D point1,
+                        Point3D point2,
+                        Point3D point3,  //вершины грани
+                        Material material)
+        {
+            GeometryModel3D geometryModel3D = new GeometryModel3D()
+            {
+                Geometry = new MeshGeometry3D()
+                {
+                    Positions = new Point3DCollection()
+                    {
+                        point1,
+                        point2,
+                        point3, //вершины треугольника
+                    }
+                },
+                Material = material //цвет грани
+            };
+            return geometryModel3D;
+        }
+        private void Draw(double size, Point3D pos)
+        {
+            // Отсчёт точек по 12 направлениям.
+            // Размерности граней симметричны в обе стороны в абсолютных величинах.
+            double absX = size / 2;
+            double absY = size / 2;
+            double absZ = size / 2;
+            Point3D[] points =
+            {
+                new Point3D(pos.X, pos.Y + absY, pos.Z),
+                new Point3D(pos.X, pos.Y, pos.Z),
+            };
+        }
+    }
+}
