5c909c07fe4c1514e17128728513267be8f6a63f
Nurgun2003 <wadul@list.ru>
2025-03-15 11:27:30 +0900

    Update Icosahedron

--- My/Icosahedron.cs
+++ My/Icosahedron.cs
@@ -70,11 +70,31 @@ namespace lab1.My
             double absX = size / 2;
             double absY = size / 2;
             double absZ = size / 2;
+            double d = Math.Atan(2);
             Point3D[] points =
             {
                 new Point3D(pos.X, pos.Y + absY, pos.Z),
-                new Point3D(pos.X, pos.Y, pos.Z),
+                new Point3D(pos.X + absX * Math.Sin(d), pos.Y + absY * Math.Cos(d), pos.Z),
+                new Point3D(pos.X + absX * Math.Sin(d) * Math.Cos(2 * Math.PI / 5), pos.Y + absY * Math.Cos(d), pos.Z - absZ * Math.Cos(d) * Math.Sin(2 * Math.PI / 5)),
+                new Point3D(pos.X + absX * Math.Sin(d) * Math.Cos(4 * Math.PI / 5), pos.Y + absY * Math.Cos(d), pos.Z - absZ * Math.Cos(d) * Math.Sin(4 * Math.PI / 5)),
+                new Point3D(pos.X + absX * Math.Sin(d) * Math.Cos(6 * Math.PI / 5), pos.Y + absY * Math.Cos(d), pos.Z - absZ * Math.Cos(d) * Math.Sin(6 * Math.PI / 5)),
+                new Point3D(pos.X + absX * Math.Sin(d) * Math.Cos(8 * Math.PI / 5), pos.Y + absY * Math.Cos(d), pos.Z - absZ * Math.Cos(d) * Math.Sin(8 * Math.PI / 5)),
+                new Point3D(pos.X + absX * Math.Sin(d) * Math.Cos(Math.PI / 5), pos.Y - absY * Math.Cos(d), pos.Z - absZ * Math.Cos(d) * Math.Sin(Math.PI / 5)),
+                new Point3D(pos.X + absX * Math.Sin(d) * Math.Cos(3 * Math.PI / 5), pos.Y - absY * Math.Cos(d), pos.Z - absZ * Math.Cos(d) * Math.Sin(3 * Math.PI / 5)),
+                new Point3D(pos.X - absX * Math.Sin(d), pos.Y - absY * Math.Cos(d), pos.Z),
+                new Point3D(pos.X + absX * Math.Sin(d) * Math.Cos(3 * Math.PI / 5), pos.Y - absY * Math.Cos(d), pos.Z + absZ * Math.Cos(d) * Math.Sin(3 * Math.PI / 5)),
+                new Point3D(pos.X + absX * Math.Sin(d) * Math.Cos(Math.PI / 5), pos.Y - absY * Math.Cos(d), pos.Z + absZ * Math.Cos(d) * Math.Sin(Math.PI / 5)),
+                new Point3D(pos.X, pos.Y - absY, pos.Z)
             };
+            Model3DGroup m3dg = new Model3DGroup();
+            // Добавление граней
+            DiffuseMaterial material = new DiffuseMaterial(RandBrush());
+            GeometryModel3D face = AddFace(points[0], points[2], points[1], material);
+        }
+        private Brush RandBrush()
+        {
+            Random rnd = new Random();
+            return new SolidColorBrush(Color.FromRgb((byte)rnd.Next(), (byte)rnd.Next(), (byte)rnd.Next()));
         }
     }
 }
